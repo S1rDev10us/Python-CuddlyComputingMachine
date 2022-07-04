e1591520d692bb9c0b10196c6ef859bd6338beb0
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2022-07-04 13:48:34 +0100

    fixed some of wojiees code, and integrated some of nates ideas

--- index.py
+++ index.py
@@ -533,12 +533,18 @@ class game:
 		with open("savedata.json", "r+") as raw:
 			data = load(raw)
 			while True:
-				self.savename = str(input("load or create save: "))
-				for i in data:
-					if self.savename == i:
-						self.save = data[i]
+				savename = str(input("Load from savename\n>>>"))
+				if savename in data:
+					print('Do you want to overwrite this file?')
+					if(self.confirm()):
+						self.save = self.savename
+						self.reset()
+						self.save = self.savescheme()
+						data[self.savename] = self.save
+						overwritejs(data, raw)
+					else:
+						self.save = data[savename]
 						self.loadSave()
-						break
 				else:
 					print("this save is not found. check the spelling or create a new save.\nwould you like to create a new save?")
 					if self.confirm():
