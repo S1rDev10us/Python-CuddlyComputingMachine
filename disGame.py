ff4bd9f7522ebc23fa8fc7767f93eb9198952511
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2023-01-30 14:39:20 +0000

    updates

---
+++ disGame.py
@@ -0,0 +1,11 @@
+from game import game
+from dis import dis
+dis(game)
+# def test1():
+# 	print('test'+str({})+'otherTest')
+# 	print('test'+{}+'otherTest')
+# 	print('test',str({}),'otherTest')
+# 	print('test',{},'otherTest')
+# 	print(f'test{str({})}otherTest')
+
+# dis(test1)
